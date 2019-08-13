0988fe94c7c43452cf86a9f67e4b9e3502ca6af3
Black <gmocore@gmail.com>
2019-08-13 13:36:22 -0600

    added code comments. updated README

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # make-it-train
+
+## Created using HTML, CSS, Bootstrap, JavaScript, jQuery, Firebase, Moment.js
+
+## Instructions
+
+- using the provided form enter the following:
+  - Train Name
+  - Destination
+  - First Train Time (in HH:mm format --must be military time)
+  - Frequency of train (in Minutes)
+- click submit button to update the page.
+
+## Code Review
+
+using an event listener for the submit button, input values are pushed to Firebase and stored. Each time a new item is pushed to the database, the database listens for a change in added children, and displays the new entry in the table display. The application is fairly simple as it consists of 2 paheses. phase one gathers and stores the data, will phase to reads the stored data and displays it the the page for the user.
+
+## Deployed Project
+
+link to deployed version of this project:
+https://gmocore.github.io/make-it-train/
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+// enables firebase access
 const firebaseConfig = {
   apiKey: "AIzaSyCz-6_0v_w6yCo3eYw5s7Yj9030kXMs32g",
   authDomain: "firetest-1f338.firebaseapp.com",
@@ -8,28 +9,36 @@ const firebaseConfig = {
   appId: "1:287293520459:web:f542392c600d89e4"
 };
 
+// sets up connection to firebase
 firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 
 $("#submit").click(function(e) {
+  // prevent page from reloading on click
   e.preventDefault();
+  // variables for form input values
   let trainNameInput = $("#train-name").val();
   let destinationInput = $("#destination").val();
   let firstTrainInput = $("#first-train-time").val();
   let frequencyInput = $("#frequency").val();
   let trainFrequency = frequencyInput;
   let firstTrainTime = moment(firstTrainInput, "HH:mm").format("HH:mm");
+  // moment varables for manipulating time
   let firstTrainTimeConverted = moment(firstTrainTime, "HH:mm").subtract(
     1,
     "years"
   );
-  let currentTime = moment();
+  // calculates difference in converted time by minutes
   let diffTime = moment().diff(moment(firstTrainTimeConverted), "minutes");
+  // remainder of difference in time divided by frequency value
   let tRemainder = diffTime % trainFrequency;
+  // calculates minutes to next train using math
   let tMinutesTillTrain = trainFrequency - tRemainder;
+  // current time + minutes until next train
   let nextTrain = moment().add(tMinutesTillTrain, "minutes");
 
+  // push stored values to database. this creates a new instance every time, and does not overwrite existing data
   database.ref().push({
     trainName: trainNameInput,
     destination: destinationInput,
@@ -38,14 +47,18 @@ $("#submit").click(function(e) {
     nextArrival: moment(nextTrain).format("hh:mm"),
     minutesAway: tMinutesTillTrain
   });
+
+  // clear inputs on submit
   trainNameInput = $("#train-name").val("");
   destinationInput = $("#destination").val("");
   firstTrainInput = $("#first-train-time").val("");
   frequencyInput = $("#frequency").val("");
 });
 
+// reads stored values in firebase
 database.ref().on("child_added", snapshot => {
   console.log(snapshot.val());
+  // variables for displaying data from firebase to the DOM
   const row = $("<tr>");
   const trainNameTd = $("<td>");
   const destinationTd = $("<td>");
@@ -53,11 +66,13 @@ database.ref().on("child_added", snapshot => {
   const nextArrivalTd = $("<td>");
   const minutesAwayTd = $("<td>");
 
+  // sets text of table data to stored firebase values
   trainNameTd.text(snapshot.val().trainName);
   destinationTd.text(snapshot.val().destination);
   frequencyTd.text(snapshot.val().frequency);
   nextArrivalTd.text(snapshot.val().nextArrival);
   minutesAwayTd.text(snapshot.val().minutesAway);
+  // append updated data to the table for the user to see on the page
   $(row).append(
     trainNameTd,
     destinationTd,
